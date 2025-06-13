1e13890ed6bed09fdd8314e376861eed78ab5feb
Sudipta <hisudipta26@gmail.com>
2025-06-13 17:43:00 +0530

    feat: Write a function to find missing number in an array

---
+++ Arrays Level-0/findMissingNumber.js
@@ -0,0 +1,19 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+const findMissingNumber = (arr) => {
+    let n = arr.length;
+    let totalSum = n * (n + 1) / 2;
+    let partialSum = 0;
+
+    for(let i = 0; i < n; i++) {
+        partialSum = partialSum + arr[i];
+    }
+
+    return totalSum - partialSum;
+}
+
+const arr = [9,6,4,2,3,5,7,0,1];
+console.log(findMissingNumber(arr));
+
+//time complexity -> O(n)
+//space complexity => O(1)
